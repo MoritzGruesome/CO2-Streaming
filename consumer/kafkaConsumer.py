e6f682ecd510cf5d5a913c97ea70e28e4882a96c
Moritz <moritzgruss@hotmail.se>
2025-11-13 23:14:27 +0100

    consumer and producer exchange has been added

--- consumer/kafkaConsumer.py
+++ consumer/kafkaConsumer.py
@@ -1,11 +1,24 @@
 from kafka import KafkaConsumer
 import time 
 
-time.sleep(20)
 print("script started")
-consumer = KafkaConsumer('exampleTopic',
-                         bootstrap_servers='kafka:9093',
-                         auto_offset_reset='earliest')
-print("consumer started")
-for msg in consumer:
-    print(msg)
+
+connectionUp = False
+
+while not connectionUp:
+
+    try:
+        consumer = KafkaConsumer('exampleTopic',
+                                bootstrap_servers='kafka:9093',
+                                auto_offset_reset='earliest')
+        print("consumer connected successfully")
+
+        connectionUp = True
+
+        for msg in consumer:
+            print(msg)
+        
+    except:
+        print("consumer failed to connect to broker")
+    time.sleep(1)
+
--- producer/kafkaProducer.py
+++ producer/kafkaProducer.py
@@ -2,10 +2,25 @@ from kafka import KafkaProducer
 import time
 
 print("producer script started")
-time.sleep(10)
-producer = KafkaProducer(bootstrap_servers='kafka:9093')
-for i in range(10):
-    producer.send('exampleTopic', b'some_message')
 
-producer.flush()
+connectionUp = False
+
+while not connectionUp:
+    try:
+
+        producer = KafkaProducer(bootstrap_servers='kafka:9093')
+
+        if producer.bootstrap_connected():
+            for i in range(10):
+                producer.send('exampleTopic', b'some_message')
+            
+            producer.flush()
+        
+        connectionUp = True
+    except:
+        print("connection to broker failed")
+    time.sleep(1)
+
+
+
 
